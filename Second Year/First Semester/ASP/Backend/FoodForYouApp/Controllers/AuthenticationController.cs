1d327ea189fe570760c3d77dae87db55a85a009d
DobreAdriana <98237679+DobreAdriana@users.noreply.github.com>
2022-03-11 18:56:56 +0200

    Add files via upload

---
+++ Second Year/First Semester/ASP/Backend/FoodForYouApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,61 @@
+﻿using FoodForYouApp.Managers;
+using FoodForYouApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodForYouApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationManager authenticationManager;
+
+        public AuthenticationController(IAuthenticationManager authenticationManager)
+        {
+            this.authenticationManager = authenticationManager;
+        }
+
+        [HttpPost("Sign-up")]
+        public async Task<IActionResult> SignUp([FromBody] RegisterModel registerModel)
+        {
+            try
+            {
+                await authenticationManager.SignUp(registerModel);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Failed to sign up!");
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            try
+            {
+                var tokens = await authenticationManager.Login(model);
+
+                if (tokens != null)
+                    return Ok(tokens);
+                else
+                {
+                    return BadRequest("Failed to login");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Failed to login in!");
+            }
+        }
+
+
+    }
+}
+
---
+++ Second Year/First Semester/ASP/Backend/FoodForYouApp/Controllers/OrderController.cs
@@ -0,0 +1,79 @@
+﻿using FoodForYouApp.Entities;
+using FoodForYouApp.Managers;
+using FoodForYouApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodForYouApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrdersManager manager;
+
+        public OrderController(IOrdersManager clientsManager)
+        {
+            this.manager = clientsManager;
+        }
+
+        [HttpGet("AllOrders")]
+        public async Task<IActionResult> GetOrders()
+        {
+            var orders = manager.GetOrders();
+
+            return Ok(orders);
+        }
+
+        [HttpGet("GetOrderbyId/{id}")]
+        public async Task<IActionResult> GetById([FromRoute] string id)
+        {
+            var order = manager.GetOrderById(id);
+
+            return Ok(order);
+        }
+
+        [HttpGet("OrdersWithFood")]
+        public async Task<IActionResult> GetOrdersWithFood()
+        {
+            var orderswithfood = manager.GetOrdersWithFood();
+
+            return Ok(orderswithfood);
+        }
+
+        [HttpPost("AddOrder")]
+        public async Task<IActionResult> Create([FromBody] OrderModel orderModel)
+        {
+            var newOrder = new Order
+            {
+                Id = orderModel.Id,
+                PaymentMethod = orderModel.PaymentMethod,
+                OrderDate = orderModel.OrderDate,
+                Address = orderModel.Address,
+                ClientId = orderModel.ClientId
+            };
+
+            manager.Create(newOrder);
+            return Ok();
+        }
+
+        [HttpPut("UpdateOrder")]
+        public async Task<IActionResult> Update([FromBody] OrderModel orderModel)
+        {
+            manager.Update(orderModel);
+
+            return Ok();
+        }
+        [HttpDelete("DeleteOrder{id}")]
+        public async Task<IActionResult> Delete([FromRoute] string id)
+        {
+            manager.Delete(id);
+
+            return Ok();
+        }
+    }
+}
